fb8e6ed3c5e7a831ad334ef8ecf8177a7b9dbd7a
nahom <nahumtefera@gmail.com>
2018-10-08 15:41:01 -0700

    created new component Article

---
+++ src/components/article/article.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './article.css';
+
+export default class Article extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
--- src/components/news/news.jsx
+++ src/components/news/news.jsx
@@ -1,32 +1,29 @@
 import React, {Component} from 'react';
 import './news.css';
+// Components
+import Article from '../article/article';
 
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('5be26c0d911a40fcac1539e471bbed2e');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.everything({
-    q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk, techcrunch.com',
-    from: '2018-9-09',
-    to: '2018-7-10',
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 2
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-}).catch((e)=>{console.log(e)});
+
+
 
 export default class News extends Component {
     constructor(props){
         super(props)
+
+        this.state = {
+            news: []
+        }
+    }
+
+    componentWillMount(){
+        newsapi.v2.topHeadlines({
+            country: 'us'
+        }).then(response => {
+            this.setState({news: response})
+            console.log(response)
+        }).catch((e)=>{console.log(e)});
     }
 
     render() {
