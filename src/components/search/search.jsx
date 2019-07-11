d69bb83c13a72d41a1034c6b09803eac4819e385
nendale <nahumtefera@gmail.com>
2019-07-11 13:51:25 -0700

    added dark-theme to search component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -47,13 +47,14 @@ export default class Search extends Component {
     }
 
     render() {
+        let nightMode = this.props.nightMode;
         return (
             <div className="search-container">
                 <div className="input-container">
-                    <input className="search-input" onKeyPress={this.handleKeyPress} type="text" placeholder="Search article"  value={this.state.query} onChange={this.handleInput} />
-                    <button onClick={this.handleSubmit} className="submit-button">Go</button>
+                    <input className={ nightMode ? "dark-search-input" : "search-input"} onKeyPress={this.handleKeyPress} type="text" placeholder="Search article"  value={this.state.query} onChange={this.handleInput} />
+                    <button onClick={this.handleSubmit} className={ nightMode ? "dark-submit-button" : "submit-button"} >Go</button>
                     <br/>
-                    <div className='attribution'>powerd by <a target="blank" href="https://newsapi.org">NewsAPI.org</a></div> {/*Attribution to Newsapi*/}
+                    <div className={nightMode ? 'dark-attribution' : 'attribution'}>powerd by <a target="blank" href="https://newsapi.org">NewsAPI.org</a></div> {/*Attribution to Newsapi*/}
                 </div>
                 <SourcesSelector region={this.props.region} submit={this.handleSubmit} updateSources={this.updateSources}/>
             </div>
